c5a522005ff631e12de4e9f37a44962982275ac5
Ting Yuan <yuanting@ict.ac.cn>
2021-12-07 18:03:45 +0800

    Schedule happens-before relation ships

--- src/runtime/chan.go
+++ src/runtime/chan.go
@@ -172,17 +172,26 @@ func makechan(t *chantype, size int, id int64) *hchan {
 	case mem == 0:
 		// Queue or element size is zero.
 		c = (*hchan)(mallocgc(hchanSize, nil, true))
+		if SyncTrapperMap.IsEnabled() && id >= 0 {
+			SyncTrapperMap.Store(c, id)
+		}
 		// Race detector uses this location for synchronization.
 		c.buf = c.raceaddr()
 	case elem.ptrdata == 0:
 		// Elements do not contain pointers.
 		// Allocate hchan and buf in one call.
 		c = (*hchan)(mallocgc(hchanSize+mem, nil, true))
+		if SyncTrapperMap.IsEnabled() && id >= 0 {
+			SyncTrapperMap.Store(c, id)
+		}
 		c.buf = add(unsafe.Pointer(c), hchanSize)
 	default:
 		// Elements contain pointers.
 		c = new(hchan)
 		c.buf = mallocgc(mem, elem, true)
+		if SyncTrapperMap.IsEnabled() && id >= 0 {
+			SyncTrapperMap.Store(c, id)
+		}
 	}
 
 	c.elemsize = uint16(elem.size)
@@ -194,9 +203,6 @@ func makechan(t *chantype, size int, id int64) *hchan {
 		print("makechan: chan=", c, "; elemsize=", elem.size, "; dataqsiz=", size, "\n")
 	}
 
-	if SyncTrapperMap.IsEnabled() && id >= 0 {
-		SyncTrapperMap.Store(c, id)
-	}
 	return c
 }
 
